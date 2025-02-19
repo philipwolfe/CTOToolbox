baf854091184606cf8ef3076305c6ee4e711bba2
Philip Wolfe <61720289+philipwolfe@users.noreply.github.com>
2025-02-19 10:57:06 -0600

    Update email_external.ps1

--- email_external.ps1
+++ email_external.ps1
@@ -1,3 +1,4 @@
+# https://techcommunity.microsoft.com/blog/exchange/native-external-sender-callouts-on-email-in-outlook/2250098
 # https://learn.microsoft.com/en-us/powershell/module/exchange/set-externalinoutlook?view=exchange-ps#examples
 #Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
